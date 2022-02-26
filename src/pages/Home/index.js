2c45e58133a45f9b0e34af2b3b2338275018aeab
cielio <cielion@hotmail.com>
2022-02-26 17:40:53 -0300

    Completo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView, Image, View, ButtonContainer } from './style';
 
-function Home() {
+
+import banner from '../../assets/img/banner.png';
+import Button from '../../components/Button';
+
+export default function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your pp!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="theme-dark" />
+      <SafeAreaView>
+        <View>
+          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Pedir comida nunca foi tão fácil</Text>
+          <Image source={banner} />
+          <Text style={{ fontWeight: 'bold', fontSize: 16 }}>Permitir Localização</Text>
+          <Text>Para descobrir restaurantes que entregam em sua região</Text>
+        </View>
+        <ButtonContainer>
+          <Button text="PULAR" onPress={() => navigation.navigate('Main')} />
+          <Button text="ENTRAR" theme="primary" />
+        </ButtonContainer>
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default Home;
