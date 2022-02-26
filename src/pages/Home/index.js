c2fd7a8d529af296f179dc493baaaa1441715414
cielio <cielion@hotmail.com>
2022-02-26 11:54:03 -0300

    Aula 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
+      <Text>Open up App.js to start working on your pp!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default Home;
